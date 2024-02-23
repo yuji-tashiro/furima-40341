47f5fa944048b9d55b589368813873a65d7695b9
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-23 20:13:03 +0900

    アクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  has_one :order
+
+  validates :name,         presence: true
+  validates :description,  presence: true
+  validates :category_id,  presence: true, numericality: { other_than: 0 }
+  validates :condition_id, presence: true, numericality: { other_than: 0 }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 0 }
+  validates :prefecture_id,   presence: true, numericality: { other_than: 0 }
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 0 }
+  validates :price,           presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_ship
 end
---
+++ app/models/shipping_charge.rb.rb
@@ -0,0 +1,10 @@
+class ShippingCharge < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' }, # 初期値
+    { id: 1, name: '着払い(購入者負担)' },
+    { id: 2, name: '送料込み(出品者負担)' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
