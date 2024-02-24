e16aaf4684b73f1ed4b417848e51dfd9f39458fd
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-24 20:35:36 +0900

    itemモデルのテストの実施

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,108 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @user = FactoryBot.create(:user) # 必要に応じてユーザーファクトリを作成してください
+    @item = FactoryBot.build(:item, user: @user)
+  end
+
+  describe 'アイテムの保存' do
+    context 'アイテムが保存できる場合' do
+      it '全ての属性が正しく入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context 'アイテムが保存できない場合' do
+      it 'imageが空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'nameが空では保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'descriptionが空では保存できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'category_idが0では保存できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
+      end
+
+      it 'condition_idが0では保存できない' do
+        @item.condition_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 0")
+      end
+
+      it 'shipping_charge_idが0では保存できない' do
+        @item.shipping_charge_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 0")
+      end
+
+      it 'prefecture_idが0では保存できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
+      end
+
+      it 'days_to_ship_idが0では保存できない' do
+        @item.days_to_ship_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 0")
+      end
+
+      it 'priceが300未満では保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9,999,999より大きいと保存できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'priceが整数でない場合、保存できない' do
+        @item.price = 300.5
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
+
+      it 'priceが全角数値では保存できないこと' do
+        @item.price = '１０００' # 全角数値
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが英字では保存できないこと' do
+        @item.price = 'abc' # 英字
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが英数字混合では保存できないこと' do
+        @item.price = '1000yen' # 英数字混合
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが記号を含む場合は保存できないこと' do
+        @item.price = '1,000' # 記号を含む
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
+end
