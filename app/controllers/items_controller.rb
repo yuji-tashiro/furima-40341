89036f29ed0e7823d6df3c186a3c0eddf99d1bb7
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-24 14:03:32 +0900

    アクティブハッシュのプルダウン実装、ログイン状態の場合のみ、商品出品ページへ遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path # トップページにリダイレクト
+    else
+      render :new # 保存に失敗した場合は、newアクションのビューを再表示
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge()
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
   
 end
