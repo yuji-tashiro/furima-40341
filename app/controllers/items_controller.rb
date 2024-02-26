3dfc7824661ddb27b311ea10016c70e6991f045a
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-26 15:53:31 +0900

    未ログインでeditアクセスでログイン画面に遷移
    ログアウト状態の場合は、商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずログインページに遷移すること。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit,]
   before_action :set_item, only: [:edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
