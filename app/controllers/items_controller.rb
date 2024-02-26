1964b9a7d0289c662b2e372c81f49978c5130f62
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-26 09:12:45 +0900

    showアクション、ルーティングの設定、show.html.erbまでのリンクを設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id,
-                                 :days_to_ship_id, :price).merge(user_id: current_user.id)
+                                :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
