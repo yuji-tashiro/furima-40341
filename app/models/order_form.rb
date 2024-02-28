f27696797a8f31451a57e7cdfc4f88e87a34f613
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-28 23:08:54 +0900

    テストコードの途中２

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,6 +1,6 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
 
   with_options presence: true do
     validates :item_id
@@ -8,10 +8,9 @@ class OrderForm
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
-    validates :street_address
+    validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
   end
-end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
