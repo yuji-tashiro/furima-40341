acad5c823ca50fc0cd4e0bba525a02db5e818bef
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-29 17:39:28 +0900

    公開鍵を環境変数化、そのほか修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,11 @@ class OrdersController < ApplicationController
   before_action :move_to_index, only: [:index, :create]
   
   def index
-    @item = Item.find(params[:item_id])
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_form = OrderForm.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     
     if @order_form.valid?
@@ -33,7 +32,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_form).permit(:item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
                               .merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_4cbe5349e790defe77423d62')// PAY.JPテスト公開鍵
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
